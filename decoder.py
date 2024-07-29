1f7ea9b9655697744495102ce64f5bb1bfee6d2b
evusr1 <127464885+evusr1@users.noreply.github.com>
2024-07-28 22:06:45 -0700

    Add instruction stub and several decoder functions

---
+++ decoder.py
@@ -0,0 +1,58 @@
+from enum import Enum
+
+class InstructionType(Enum):
+    R = 1
+    I = 2
+    S = 3
+    B = 4
+    U = 5
+    J = 6
+
+class InstructionTypeOp(Enum):
+    R = 0b0110011
+    I = 0b0010011
+    I2 = 0b0000011
+    S = 0b0100011
+    B = 0b1100011
+    J = 0b1101111
+    I3 = 0b1100111
+    U = 0b0110111
+    U2 = 0b0010111
+    I4 = 0b1110011
+
+def op(instruction):
+    return instruction & 0b1111111
+
+def get_type(instruction):
+    opcode = op(instruction)
+
+    match opcode:
+        case InstructionTypeOp.R:
+            return InstructionType.R
+        case InstructionTypeOp.I | InstructionTypeOp.I2 | InstructionTypeOp.I3 | InstructionTypeOp.I4:
+            return InstructionType.I
+        case InstructionTypeOp.S:
+            return InstructionType.S
+        case InstructionTypeOp.B:
+            return InstructionType.B
+        case InstructionTypeOp.J:
+            return InstructionType.J
+        case InstructionTypeOp.U | InstructionTypeOp.U2:
+            return InstructionType.U
+
+
+def func3(instruction):
+    return instruction & (0b111 << 11)
+
+def rd(instruction):
+    return instruction & (0b11111 << 6)
+
+def rs1(instruction):
+    return instruction & (0b11111 << 14)
+
+def rs2(instruction):
+    return instruction & (0b11111 << 19)
+
+
+def decode(instruction):
+    pass
---
+++ instruction.py
@@ -0,0 +1,124 @@
+def add(rd, rs1, rs2):
+    pass
+
+def sub(rd, rs1, rs2):
+    pass
+
+def xor(rd, rs1, rs2):
+    pass
+
+def or_:(rd, rs1, rs2)
+    pass
+
+def and_(rd, rs1, rs2):
+    pass
+
+def sll(rd, rs1, rs2):
+    pass
+
+def srl(rd, rs1, rs2):
+    pass
+
+def sra(rd, rs1, rs2):
+    pass
+
+def slt(rd, rs1, rs2):
+    pass
+
+def sltu(rd, rs1, rs2):
+    pass
+
+
+def addi(rd, rs1, imm):
+    pass
+
+def xori(rd, rs1, imm):
+    pass
+
+def ori(rd, rs1, imm):
+    pass
+
+def andi(rd, rs1, imm):
+    pass
+
+def slli(rd, rs1, imm):
+    pass
+
+def srli(rd, rs1, imm):
+    pass
+
+def srai(rd, rs1, imm):
+    pass
+
+def slti(rd, rs1, imm):
+    pass
+
+def sltiu(rd, rs1, imm):
+    pass
+
+
+def lb(rd, imm):
+    pass
+
+def lh(rd, imm):
+    pass
+
+def lw(rd, imm):
+    pass
+
+def lbu(rd, imm):
+    pass
+
+def lhu(rd, imm):
+    pass
+
+
+def sb(imm, rs2):
+    pass
+
+def sh(imm, rs2):
+    pass
+
+def sw(imm, rs2):
+    pass
+
+
+def beq(imm, rs1, rs2):
+    pass
+
+def bne(imm, rs1, rs2):
+    pass
+
+def blt(imm, rs1, rs2):
+    pass
+
+def bge(imm, rs1, rs2):
+    pass
+
+def bltu(imm, rs1, rs2):
+    pass
+
+def bgeu(imm, rs1, rs2):
+    pass
+
+
+def jal(rd, imm):
+    pass
+
+def jal(rd, rs1, imm):
+    pass
+
+
+def lui(rd, imm):
+    pass
+
+def auipc(rd, imm):
+    pass
+
+
+def ecall():
+    pass
+
+def ebreak():
+    pass
+
